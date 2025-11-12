1406e05dfbec013bafb23447771cc10c234e4150
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2025-11-12 16:24:38 +0100

    défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
 // *** CAROUSEL ***
 // TODO
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+if (carousel) {
+  // Scroll au clic sur le bouton précédent
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({left:-260, behavior:"smooth"});
+  });
+
+  // Scroll au clic sur le bouton suivant
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({left:+260, behavior:"smooth"});
+  });
+}
